2d44f6d59201621269ad6b5d389b61c3ea83acb1
Sardred <andelar96@gmail.com>
2017-01-13 20:09:02 +0100

    Initial Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+};
