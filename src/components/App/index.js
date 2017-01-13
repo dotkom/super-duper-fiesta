3d1142f1a660e0e21a32c800fb1656a10c522ade
Torjus Iveland <torjusti@gmail.com>
2017-01-13 19:52:06 +0100

    Add ActiveIssue component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ActiveIssue from '../ActiveIssue';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Hello world!
+        <ActiveIssue issue="Hello world" />
       </div>
     );
   }
