6503e258d073958b82d18807889c2e616c002b4e
Torjus Iveland <torjusti@gmail.com>
2017-01-13 23:25:37 +0100

    Add eslint configuration

---
+++ src/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: "airbnb",
+
+  globals: {
+    Urls: true,
+  },
+
+  env: {
+    browser: true,
+    jest: true,
+  },
+
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [
+          ".js",
+          ".jsx",
+        ],
+      },
+    ],
+  },
+
+  settings: {
+    "import/resolver": {
+      webpack: {
+        config: "webpack.config.js",
+      },
+    },
+  },
+};
