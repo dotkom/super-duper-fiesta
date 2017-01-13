a182f2782bd7dd22a6c8bd6d72bff8adfc3f6e18
Torjus Iveland <torjusti@gmail.com>
2017-01-13 21:37:37 +0100

    Connect ActiveIssue to Redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ActiveIssue from "../ActiveIssue";
-import LiveCount from "../LiveCount";
+import React from 'react';
+import ActiveIssue from  '../../containers/ActiveIssue';;
+import LiveCount from '../LiveCount';
 
 const App = () => {
   return (
     <div className="App">
-      <ActiveIssue issue="Hello world"/>
+      <ActiveIssue />
       <LiveCount voteCount="3" userCount="20"/>
     </div>
   );
--- src/components/Issue/index.js
+++ src/components/Issue/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default ({ issue }) => (
   <div className="ActiveIssue">
     <h3>Aktiv sak</h3>
-    <p>{issue}</p>
+    <p>{ issue }</p>
   </div>
 );
