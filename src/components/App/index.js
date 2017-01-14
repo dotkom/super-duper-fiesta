7019eb31c53da2d5ea92d4731d9104390ba6b7a1
Torjus Iveland <torjusti@gmail.com>
2017-01-14 15:44:42 +0100

    Create container for issue count

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import LiveCount from '../LiveCount';
+import LiveVoteCount from '../../containers/LiveVoteCount';
 import VoteHandler from '../../containers/VoteHandler';
 import ActiveIssue from '../../containers/ActiveIssue';
-
 
 const App = () => (
   <div className="App">
     <ActiveIssue />
-    <LiveCount voteCount={3} userCount={20} />
+    <LiveVoteCount />
     <VoteHandler />
   </div>
   );
