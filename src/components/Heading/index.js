1701e4804d3d4c27a9c692952127e126ec72df5c
nornes <andrehn@stud.ntnu.no>
2017-01-14 16:38:43 +0100

    add style loaders and start styling heading

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Heading.css';
 
 export default () => (
-  <div className="HeadingContainer">
-    <h2>Onlines generalforsamling 2017</h2>
-    <a className="gf_logout" href="/genfors/logout/">Logg ut</a>
+  <div className="Heading">
+    <h2 className="Heading-header">Onlines generalforsamling 2017</h2>
+    <a className="Heading-logout" href="/genfors/logout/">Logg ut</a>
   </div>
 );
