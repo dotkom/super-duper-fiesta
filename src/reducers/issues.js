6767ebbaed5b271fa81cebc7dae507245fbe1cba
nornes <andrehn@stud.ntnu.no>
2017-01-14 20:01:46 +0100

    add sample closed issues

--- src/reducers/issues.js
+++ src/reducers/issues.js
@@ -64,6 +64,42 @@ const defaultIssues = [{
 
   majorityTreshold: 0.5,
   isActive: false,
+}, {
+  id: 1,
+  text: 'YO hello this is long text',
+  alternatives: [{
+    id: 0,
+    text: 'yes',
+  }, {
+    id: 1,
+    text: 'yes',
+  }, {
+    id: 2,
+    text: 'yes',
+  }],
+
+  votes: [],
+
+  majorityTreshold: 0.5,
+  isActive: false,
+}, {
+  id: 2,
+  text: 'Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire',
+  alternatives: [{
+    id: 0,
+    text: 'yes',
+  }, {
+    id: 1,
+    text: 'yes',
+  }, {
+    id: 2,
+    text: 'yes',
+  }],
+
+  votes: [],
+
+  majorityTreshold: 0.5,
+  isActive: false,
 }];
 
 const issues = (state = defaultIssues, action) => {
