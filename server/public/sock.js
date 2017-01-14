0b148642725b9ad0d50ad0d15b26b0eec0ec853a
sklirg <hlsolbjorg@gmail.com>
2017-01-14 21:12:28 +0100

    Eslint most of this dummy script

--- server/public/sock.js
+++ server/public/sock.js
@@ -1,18 +1,16 @@
-var socket = io.connect('http://localhost:3000')
-socket.on('connection', function (data) {
-  console.log('connected')
-})
-socket.on('meeting', function (data) {
+/* global: io */
+const socket = io.connect('http://localhost:3000');
+socket.on('connection', () => {});
+socket.on('meeting', (data) => {
   let title = 'Ingen aktiv generalforsamling.';
-  console.log('d', data)
 
   if (data && data.title) {
     title = data.title;
   }
   document.getElementById('meeting-title').innerHTML = title;
 
-  socket.emit('my other event', { my: 'data' })
-  const node = document.createElement('p')
-  node.innerHTML = title
-  document.getElementById('log').appendChild(node)
+  socket.emit('my other event', { my: 'data' });
+  const node = document.createElement('p');
+  node.innerHTML = title;
+  document.getElementById('log').appendChild(node);
 });
