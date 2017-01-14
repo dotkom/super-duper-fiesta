d3142bc5688a38d9cc4a58f25dbcec4c6c77ed24
sklirg <hlsolbjorg@gmail.com>
2017-01-14 14:27:35 +0100

    Add some dummy code

--- server/app.js
+++ server/app.js
@@ -14,15 +14,33 @@ app.get('/', function (req, res) {
 		})
 })
 
+/* Get the current active meeting */
+app.get('/genfors', function(req, res) {
+	// var meeting = getActiveMeeting()
+	// if (!meeting) { res.json({ error: 1, code: 'no_active_meeting', message: 'Ingen aktiv generalforsamling.' }) }
+	res.json({ title: 'Generalforsamlingen 2017', date: new Date(1484395200000) })
+})
+
+/* Used to fetch historical questions and their results */
 app.get('/questions', function(req, res) {
 	res.json([ { title: 'temporary question title' } ])
 })
 
 io.on('connection', function (socket) {
-  socket.broadcast.emit('news', { hello: 'world', message: "Hello world!" });
+	// Some dummy code
+	// Send an event to whoever connected
+	socket.emit('private', { message: 'Hey ;)' })
+	// Broadcast an event to all connections
+  socket.broadcast.emit('public', { hello: 'world', message: "Hello world!" });
+	// Do something when we receive this kind of event
   socket.on('my other event', function (data) {
     console.log(data);
   });
+	// End dummy code
+
+	// var meeting = getActiveMeeting()
+	// if (!meeting) { socket.emit('meeting', { error: 1, code: 'no_active_meeting', message: 'Ingen aktiv generalforsamling.' }) }
+	socket.emit('meeting', { title: 'Some meeting' })
 });
 
 server.listen(3000, function () {
--- server/public/sock.js
+++ server/public/sock.js
@@ -1,9 +1,11 @@
-console.log('lelelasl')
 var socket = io.connect('http://localhost:3000')
-socket.on('news', function (data) {
+socket.on('connection', function (data) {
+  console.log('connected')
+})
+socket.on('meeting', function (data) {
   console.log(data)
   socket.emit('my other event', { my: 'data' })
   var node = document.createElement('p')
-  node.innerHTML = data.message
+  node.innerHTML = data.title
   document.getElementById('log').appendChild(node)
 });
