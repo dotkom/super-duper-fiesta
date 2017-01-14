68cb0ae30c095a626d279aef890f567b025b6ec0
Christian Duvholt <cxhristian@gmail.com>
2017-01-14 23:17:09 +0100

    Gray header, switch body background and issue background

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,7 +3,9 @@ import './Heading.css';
 
 export default () => (
   <div className="Heading">
-    <h2 className="Heading-header">Onlines generalforsamling 2017</h2>
-    <a className="Heading-logout" href="/genfors/logout/">Logg ut</a>
+    <div className="Heading-content">
+      <h2 className="Heading-header">Onlines generalforsamling 2017</h2>
+      <a className="Heading-logout" href="/genfors/logout/">Logg ut</a>
+    </div>
   </div>
 );
