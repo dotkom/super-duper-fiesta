f8c567585268a9cc449c9924e32730814206fb2c
sklirg <hlsolbjorg@gmail.com>
2017-01-15 22:20:46 +0100

    Get active issue on connect

--- server/channels/connection.js
+++ server/channels/connection.js
@@ -3,24 +3,25 @@ const emit = require('../utils').emit;
 const logger = require('../logging');
 
 const getActiveGenfors = require('../models/meeting').getActiveGenfors;
+const getActiveQuestion = require('../models/issue').getActiveQuestion;
 
 const connection = (socket) => {
-  // Some dummy code
-  // Send an event to whoever connected
-  socket.emit('private', { message: 'Hey ;)' });
-  // Broadcast an event to all connections
-  socket.broadcast.emit('public', { hello: 'world', message: 'Hello world!' });
-  // Do something when we receive this kind of event
-  socket.on('my other event', (data) => {
-    logger.debug(data);
-  });
-  // End dummy code
-
   getActiveGenfors().then((meeting) => {
     if (!meeting) {
       socket.emit('meeting', { error: 1, code: 'no_active_meeting', message: 'Ingen aktiv generalforsamling.' });
     } else {
       socket.emit('meeting', meeting);
+      getActiveQuestion(meeting._id).then((issue) => {
+        logger.debug('Current issue', { issue: issue.description });
+        emit(socket, 'issue', issue, { action: issue.active ? 'open' : 'close' });
+      }).catch((err) => {
+        logger.error('Getting currently active issue failed.', { err });
+        emit(socket, 'issue', {}, {
+          action: null,
+          error: null,
+          message: 'Det er ingen aktive saker for øyeblikket.',
+        });
+      });
     }
   }).catch((err) => {
     logger.error('Something went wrong.', { err });
