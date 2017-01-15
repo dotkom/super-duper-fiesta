47cd78935ca6f5f74c385b4cc43f5bf4fbec5394
nornes <andrehn@stud.ntnu.no>
2017-01-15 14:54:38 +0100

    convert ConcludedIssue to class

--- src/components/ConcludedIssue/index.js
+++ src/components/ConcludedIssue/index.js
@@ -2,24 +2,44 @@ import React from 'react';
 import classNames from 'classnames';
 import './ConcludedIssue.css';
 
-const ConcludedIssue = ({ text, alternatives, votes, majorityTreshold, totalUsers }) => (
-  <div className="ConcludedIssue">
-    <p>{text}</p>
-    <ul>
-      {alternatives.map(alternative => (
-        <li
-          key={alternative.id}
-          className={classNames({
-            winner: votes.length && votes
-              .filter(vote => vote.alternative === alternative.id)
-              .length / votes.length >= majorityTreshold,
-          })}
-        >
-          {alternative.text}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+
+class ConcludedIssue extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleAlternatives: false,
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      visibleAlternatives: this.state.visibleAlternatives,
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="ConcludedIssue">
+        <p>{this.props.text}</p>
+        <ul>
+          {this.props.alternatives.map(alternative => (
+            <li
+              key={alternative.id}
+              className={classNames({
+                winner: this.props.votes.length && this.props.votes
+                  .filter(vote => vote.alternative === alternative.id)
+                  .length / this.props.votes.length >= this.props.majorityTreshold,
+              })}
+            >
+              {alternative.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
 
 export default ConcludedIssue;
