2858dea47acd4acdbd81f47ec2bb36c13a25efd3
sklirg <hlsolbjorg@gmail.com>
2017-01-15 19:39:10 +0100

    Make closing questions work. Also refine some logging.

--- server/channels/issue.js
+++ server/channels/issue.js
@@ -2,24 +2,48 @@ const logger = require('../logging');
 
 const addQuestion = require('../helpers').addQuestion;
 const endQuestion = require('../helpers').endQuestion;
+const getUserById = require('../helpers').getUserById;
 
 const issue = (socket) => {
   socket.on('issue', (data) => {
-    logger.debug('issue data', data);
+    const payload = data.data;
+    logger.debug('issue payload', { payload, action: data.action });
     if (data.action === 'open') {
-      logger.info('received issue open');
       addQuestion(data)
       .then((question) => {
-        logger.debug('add question works', { question: question.description });
+        logger.debug('Added new question. Broadcasting ...', { question: question.description });
         socket.broadcast.emit('issue', question);
+        return null;
       }).catch((err) => {
-        logger.error('add question error on socket', { err });
+        logger.error('Adding new question failed.', { err });
+        return null;
       });
+      return null;
+    } else if (data.action === 'close') {
+      if (!data.user) {
+        socket.emit('issue', {
+          error: 'User id required to be able to close an ongoing issue.',
+        });
+        return null;
+      }
+      logger.info('Closing issue.', { issue: data._id, user: data.user });
+      getUserById(data.user).then((user) => {
+        logger.debug('Fetched user profile', { user: user.name });
+        endQuestion(data._id, user)
+        .catch((err) => {
+          logger.error('closing issue failed', { err });
+        }).then((d) => {
+          logger.info('closed question', { question: d._id });
+        });
+        socket.broadcast.emit('issue', data);
+        return null;
+      }).catch((err) => {
+        logger.error('getting user failed', { err });
+        return null;
+      });
+      return null;
     }
-    if (data.action === 'close') {
-      logger.info('received issue close');
-      socket.broadcast.emit('issue', data);
-    }
+    return null;
   });
 };
 
