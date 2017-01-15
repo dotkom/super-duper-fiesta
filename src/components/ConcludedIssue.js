ffe40beddf09430a182330748322088a4cd6f51a
nornes <andrehn@stud.ntnu.no>
2017-01-15 16:02:55 +0100

    Make the alternatives for concluded issues show up and hide when issue is clicked and add class to issues that did not get majority

--- src/components/ConcludedIssue.js
+++ src/components/ConcludedIssue.js
@@ -4,25 +4,46 @@ import '../css/ConcludedIssue.css';
 
 
 class ConcludedIssue extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       visibleAlternatives: false,
+      majority: this.calculateMajority(props.alternatives, props.votes, props.majorityTreshold),
     }
+    this.handleClick = this.handleClick.bind(this);
   }
 
+
+  // Maps over alternatives to see if any of them got majority vote
+  calculateMajority(alternatives, votes, majorityTreshold) {
+    let majority = false;
+    alternatives.map(function(alternative){
+      if (votes
+        .filter(vote => vote.alternative === alternative.id)
+        .length / votes.length >= majorityTreshold) {
+        majority = true
+      }
+    });
+    return majority;
+  }
+
+
+  // CLicking the issue should show/hide the answers
   handleClick() {
     this.setState({
-      visibleAlternatives: this.state.visibleAlternatives,
-    })
+      visibleAlternatives: !this.state.visibleAlternatives,
+    });
   }
 
 
   render() {
+    console.log(this.state.visibleAlternatives)
     return (
-      <div className="ConcludedIssue">
+      <div className={classNames("ConcludedIssue", {'ConcludedIssue-NotMajority': this.state.majority})} onClick={this.handleClick}>
         <p>{this.props.text}</p>
-        <ul>
+        <ul className={classNames({"hidden": !this.state.visibleAlternatives})}>
           {this.props.alternatives.map(alternative => (
             <li
               key={alternative.id}
