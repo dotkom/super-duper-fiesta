b12304827eab984d7806d371e52a020409065083
sklirg <hlsolbjorg@gmail.com>
2017-01-19 01:59:21 +0100

    Allow for hot reloading react code using backend server

--- server/app.js
+++ server/app.js
@@ -10,12 +10,21 @@ require('./channels/index').listen(server);
 
 require('./models/essentials');
 
-app.use('/dist', express.static('../dist'));
+if (process.env.PRODUCTION) {
+  const staticDir = './dist';
+  logger.info(`Serving staticfiles from ${staticDir}`);
+  app.use('/dist', express.static(staticDir));
+} else {
+  const webpackDevMiddleware = require('./webpack-dev-middleware'); // eslint-disable-line global-require
+  logger.info('Starting webpack hot-reloading client');
+  app.use(webpackDevMiddleware);
+}
+
 app.use('/', routes);
 
 const HOST = process.env.SDF_HOST || 'localhost';
 const PORT = process.env.SDF_PORT || 3000;
 
 server.listen(PORT, HOST, () => {
-  logger.info('Webserver running on %s:%s', HOST, PORT);
+  logger.info('Running super-duper-fiesta on %s:%s', HOST, PORT);
 });
