afda39743e561aeab44ed18bfe95c9caf3d44537
Christian Duvholt <cxhristian@gmail.com>
2017-02-17 23:17:24 +0100

    Add Dialog component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import Button from './Button';
 import Heading from './Heading';
 import ConcludedIssueListContainer from '../containers/ConcludedIssueListContainer';
 import '../css/App.css';
+import '../css/flaticon.css';
 
 const App = props => (
   <div className="App">
