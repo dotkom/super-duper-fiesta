eadf7160e407cbd7bcf05ed362adbb37ff73b6da
Christian Duvholt <cxhristian@gmail.com>
2017-03-05 02:11:30 +0100

    New transparent button style

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,8 +2,12 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import '../css/Button.css';
 
-const Button = ({ size, children, ...other }) => {
-  const buttonClass = classNames('Button', `Button--size-${size}`);
+const Button = ({ size, children, background, ...other }) => {
+  const buttonClass = classNames({
+    'Button': true,
+    [`Button--size-${size}`]: size,
+    'Button--background': background,
+  });
   return (
     <button className={buttonClass} {...other}>{children}</button>
   );
@@ -11,10 +15,12 @@ const Button = ({ size, children, ...other }) => {
 
 Button.defaultProps = {
   size: 'md',
+  background: false,
 };
 
 
 Button.propTypes = {
+  background: PropTypes.bool,
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
