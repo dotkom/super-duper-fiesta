e3d77f590a5d02849b462757ad9c68a0979ed19d
Christian Duvholt <cxhristian@gmail.com>
2017-03-08 21:55:41 +0100

    Add PIN component

---
+++ client/src/components/Pin.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+import '../css/Pin.css';
+
+const Pin = ({ code }) => (
+  <div className="Pin">
+    <b>Gjeldende PIN kode</b>: {code}
+  </div>
+);
+
+Pin.propTypes = {
+  code: PropTypes.string.isRequired,
+};
+
+export default Pin;
