b3296cc503572e333b621d1d164eace0c40ffe9a
sklirg <hlsolbjorg@gmail.com>
2017-03-27 10:58:16 +0200

    Provide default key so the app starts without confing oauth2

--- server/auth/conf.js
+++ server/auth/conf.js
@@ -3,9 +3,9 @@ const OW4_OAUTH2_PROVIDER_BACKEND = process.env.SDF_OAUTH2_PROVIDER_BACKEND || '
 const OW4OAUTH2_SETUP = {
   authorizationURL: OW4_OAUTH2_PROVIDER_BACKEND + process.env.SDF_OAUTH2_AUTHORIZATION_URL || '/sso/o/authorize/',
   tokenURL: OW4_OAUTH2_PROVIDER_BACKEND + process.env.SDF_OAUTH2_TOKEN_URL || '/sso/o/token/',
-  clientID: process.env.SDF_OAUTH2_CLIENT_ID || '',
+  clientID: process.env.SDF_OAUTH2_CLIENT_ID || 'default_key',
   clientSecret: process.env.SDF_OAUTH2_CLIENT_SECRET || '',
-  callbackURL: process.env.SDF_OAUTH2_CALLBACK_URL || '',
+  callbackURL: process.env.SDF_OAUTH2_CALLBACK_URL || 'http://127.0.0.1:3000/auth',
   scope: ['authentication.onlineuser.username.read',
     'authentication.onlineuser.first_name.read',
     'authentication.onlineuser.last_name.read',
