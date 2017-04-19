c0223ad617961b5e3cda7499e32ed2cd39d937eb
Torjus Iveland <torjusti@gmail.com>
2017-04-19 11:10:41 +0200

    Refactor ConcludedIssue to be a pure component

--- client/src/components/ConcludedIssueList.js
+++ client/src/components/ConcludedIssueList.js
@@ -4,10 +4,28 @@ import ConcludedIssue from './ConcludedIssue';
 import css from './ConcludedIssueList.css';
 import { getConcludedIssues } from '../selectors/issues';
 
+// Maps over alternatives to see if any of them got majority vote
+const calculateMajority = (alternatives, votes, voteDemand) => {
+  const numTotalVotes = Object.keys(votes).length;
+  return alternatives.any(alternative => Object.keys(votes)
+    .map(key => votes[key])
+    .filter(vote => vote.alternative === alternative.id)
+    .length / numTotalVotes >= voteDemand,
+  );
+};
+
 const ConcludedIssueList = ({ issues }) => (
   <div className={css.concludedIssueList}>
     {Object.keys(issues).map(issue => (
-      <ConcludedIssue key={issues[issue].id} {...issues[issue]} />
+      <ConcludedIssue
+        key={issues[issue].id}
+        majority={calculateMajority(
+          issues[issue].alternatives,
+          issues[issue].votes,
+          issues[issue].voteDemand,
+        )}
+        {...issues[issue]}
+      />
     ))}
   </div>
 );
