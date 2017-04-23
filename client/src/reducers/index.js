6614e311221208db96bf4c82fd1f9a5fd71715b7
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-04-23 23:16:45 +0200

    Show warning if application has new version (#82)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import version from './version';
 import auth from './auth';
 import issues from './issues';
 import meeting from './meeting';
@@ -18,6 +19,7 @@ const votingApp = combineReducers({
   registrationEnabled,
   userFilter,
   users,
+  version,
   error,
 });
 
