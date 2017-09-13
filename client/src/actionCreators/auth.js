5aa8331f2c3d342f5ddabe520dfa623891a36ecc
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-09-13 16:39:46 +0200

    Add action for admin password event

--- client/src/actionCreators/auth.js
+++ client/src/actionCreators/auth.js
@@ -1,4 +1,4 @@
-import { AUTH_SIGNED_OUT, AUTH_REGISTER } from '../../../common/actionTypes/auth';
+import { ADMIN_LOGIN, AUTH_SIGNED_OUT, AUTH_REGISTER } from '../../../common/actionTypes/auth';
 
 export const register = (pin, passwordHash) => ({
   type: AUTH_REGISTER,
@@ -9,3 +9,8 @@ export const register = (pin, passwordHash) => ({
 export const signOut = () => ({
   type: AUTH_SIGNED_OUT,
 });
+
+export const adminLogin = (password) => ({
+  type: ADMIN_LOGIN,
+  password,
+})
