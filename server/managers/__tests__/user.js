35e073193f6dd7ccc4d24fe82a1ac0a05a40c6b8
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-10-22 02:27:21 +0200

    Add more tests for user manager

--- server/managers/__tests__/user.js
+++ server/managers/__tests__/user.js
@@ -1,7 +1,53 @@
+jest.mock('../../models/meeting');
 jest.mock('../../models/user');
-const { updateUserById } = require('../../models/user');
-const { setUserPermissions } = require('../user');
+const { addUser: modelAddUser, updateUserById } = require('../../models/user');
+const { getActiveGenfors } = require('../../models/meeting');
+const { addUser, setUserPermissions } = require('../user');
 const permissionLevels = require('../../../common/auth/permissions');
+const { generateGenfors, generateUser } = require('../../utils/generateTestData');
+
+const userObj = generateUser({
+  name: 'Test User',
+  onlinewebId: 'testuser1',
+  permissions: permissionLevels.CAN_VOTE,
+});
+
+describe('addUser manager', () => {
+  beforeAll(() => {
+    modelAddUser.mockImplementation(data =>
+      generateUser({
+        name: data.name,
+        onlinewebId: data.onlinewebId,
+        permissions: data.permissions,
+      }));
+    getActiveGenfors.mockImplementation(async () => generateGenfors());
+  });
+
+  it('adds a new user', async () => {
+    expect(await addUser(userObj.name, userObj.onlinewebId, userObj.permissions))
+      .toMatchObject(userObj);
+  });
+
+  it('doesnt add a new user if no genfors and not superuser', async () => {
+    getActiveGenfors.mockImplementation(async () => null);
+
+    let err;
+
+    try {
+      await addUser(userObj.name, userObj.onlinewebId, userObj.permissions);
+    } catch (thrownErr) {
+      err = thrownErr;
+    }
+    expect(err).toEqual(new Error('Ingen aktive generalforsamlinger'));
+  });
+
+  it('adds a new user if no genfors but user is superuser', async () => {
+    getActiveGenfors.mockImplementation(async () => null);
+
+    expect(await addUser(userObj.name, userObj.onlinewebId, permissionLevels.IS_SUPERUSER))
+      .toMatchObject(Object.assign(userObj, { permissions: permissionLevels.IS_SUPERUSER }));
+  });
+});
 
 describe('updating user permissions', () => {
   it('returns updated user object', async () => {
