1a1ef759618ce3fbe9d0dfc2fa8504134c6074ac
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-10-24 17:59:26 +0200

    Make back end handle incoming end meeting action

--- server/channels/admin/meeting.js
+++ server/channels/admin/meeting.js
@@ -2,11 +2,19 @@ const emit = require('../../utils').emit;
 const logger = require('../../logging');
 
 const { getActiveGenfors } = require('../../models/meeting');
-const { toggleRegistrationStatus } = require('../../managers/meeting');
+const { endGenfors, toggleRegistrationStatus } = require('../../managers/meeting');
 
 const {
+  ADMIN_END_MEETING,
   TOGGLE_REGISTRATION_STATE,
   TOGGLED_REGISTRATION_STATE } = require('../../../common/actionTypes/meeting');
+
+
+async function endGAM(socket) {
+  const genfors = await getActiveGenfors();
+  logger.info('Ending meeting', { genfors: genfors.title });
+  await endGenfors(genfors, socket.request.user);
+}
 
 const toggleRegistration = async (socket, data) => {
   logger.debug('Toggling meeting registration status', data);
@@ -21,6 +29,10 @@ const listener = (socket) => {
     switch (data.type) {
       case TOGGLE_REGISTRATION_STATE: {
         toggleRegistration(socket, data);
+        break;
+      }
+      case ADMIN_END_MEETING: {
+        endGAM(socket);
         break;
       }
       default:
