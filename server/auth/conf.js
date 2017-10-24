c04e183f425c235b04e16de683296b87a2afbbf7
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-10-24 17:21:29 +0200

    Remove logging of OAuth2 client ID and client secret

--- server/auth/conf.js
+++ server/auth/conf.js
@@ -20,8 +20,7 @@ const OW4OAUTH2_SETUP = {
     'authentication.onlineuser.rfid.read'].join(' '),
 };
 
-logger.info('Running OAuth2 with the following settings.',
-  Object.assign({}, OW4OAUTH2_SETUP, { OW4_OAUTH2_PROVIDER_BACKEND }));
+logger.info(`Running OAuth2 against ${OW4_OAUTH2_PROVIDER_BACKEND}`);
 
 module.exports = {
   ids: OW4OAUTH2_SETUP,
