de0b0dfc180bc0509135c178bd3930b001704aec
Christian Duvholt <christian@duvholt.net>
2017-10-26 15:48:19 +0200

    Remove new Promise and use async/await instead

--- server/managers/vote.js
+++ server/managers/vote.js
@@ -4,46 +4,43 @@ const permissionLevel = require('../../common/auth/permissions');
 const { getIssueById } = require('../models/issue');
 const { haveIVoted, createVote } = require('../models/vote');
 
-function addVote(issueId, user, alternative, voter) {
-  return new Promise((resolve, reject) => {
-    getIssueById(issueId)
-    .then((issue) => {
-      logger.debug('Storing vote.', { issueId, user: user.onlinewebId });
-      if (!issue.active) {
-        logger.warn('Tried to vote on inactive issue!', { issueId, user: user.onlinewebId });
-        reject(new Error('Saken du stemte på er ikke lenger aktiv'));
-        return;
-      }
-      if (!user.canVote) {
-        logger.warn('Tried to vote without the right to vote!', { issueId, user: user.onlinewebId });
-        reject(new Error('Du har ikke stemmerett'));
-        return;
-      }
-      logger.silly('Checking permissions.', { issueId, user: user.onlinewebId });
-      canEdit(permissionLevel.CAN_VOTE, user, issue.genfors).then(async () => {
-        const alreadyVoted = await haveIVoted(issueId, voter);
-        if (!alreadyVoted) {
-          const vote = createVote(voter, issueId, alternative);
-          logger.debug('Storing vote.', { issueId, user: user.onlinewebId, voter });
-          vote.save().then(resolve).catch(reject);
-        } else {
-          logger.debug('User has already voted!', { issueId, user: user.onlinewebId, voter });
-          reject(new Error('Du har allerede stemt.'));
-        }
-      }).catch((err) => {
-        logger.warn('User tried to vote without having appropriate permissions.', {
-          issueId,
-          user: user.onlinewebId,
-          requiredPerms: permissionLevel.CAN_VOTE,
-          hadPerms: user.permissions,
-        });
-        reject(err);
-      });
-    }).catch((err) => {
-      logger.error('Retrieving issue for vote failed!', err, { issueId });
-      reject(new Error('Noe gikk galt. Vennligst prøv igjen.'));
+async function addVote(issueId, user, alternative, voter) {
+  let issue;
+  try {
+    issue = await getIssueById(issueId);
+  } catch (err) {
+    logger.error('Retrieving issue for vote failed!', err, { issueId });
+    throw new Error('Noe gikk galt. Vennligst prøv igjen.');
+  }
+  logger.debug('Storing vote.', { issueId, user: user.onlinewebId });
+  if (!issue.active) {
+    logger.warn('Tried to vote on inactive issue!', { issueId, user: user.onlinewebId });
+    throw new Error('Saken du stemte på er ikke lenger aktiv');
+  }
+  if (!user.canVote) {
+    logger.warn('Tried to vote without the right to vote!', { issueId, user: user.onlinewebId });
+    throw new Error('Du har ikke stemmerett');
+  }
+  logger.silly('Checking permissions.', { issueId, user: user.onlinewebId });
+  try {
+    await canEdit(permissionLevel.CAN_VOTE, user, issue.genfors);
+    const alreadyVoted = await haveIVoted(issueId, voter);
+    if (!alreadyVoted) {
+      const vote = createVote(voter, issueId, alternative);
+      logger.debug('Storing vote.', { issueId, user: user.onlinewebId, voter });
+      return vote.save();
+    }
+    logger.debug('User has already voted!', { issueId, user: user.onlinewebId, voter });
+    throw new Error('Du har allerede stemt.');
+  } catch (err) {
+    logger.warn('User tried to vote without having appropriate permissions.', {
+      issueId,
+      user: user.onlinewebId,
+      requiredPerms: permissionLevel.CAN_VOTE,
+      hadPerms: user.permissions,
     });
-  });
+    throw err;
+  }
 }
 
 const getPublicVote = (vote, secret, showOnlyWinner) => ({
