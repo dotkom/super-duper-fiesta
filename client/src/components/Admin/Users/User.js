482730d3885ebbde4e6c15ae41b399fc267042a5
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-10-31 14:31:52 +0100

    Rewrite User to class based Component

--- client/src/components/Admin/Users/User.js
+++ client/src/components/Admin/Users/User.js
@@ -4,61 +4,68 @@ import moment from 'moment';
 import css from './User.css';
 import { CAN_VOTE, getPermissionDisplay } from '../../../../../common/auth/permissions';
 
-const User = ({
-    id, name, registered, canVote, completedRegistration, permissions, toggleCanVote,
-  }) => {
-  const userClass = classNames({
-    [css.canNotVote]: !canVote,
-  });
-  const registeredDate = moment(registered);
-  const successToggle = classNames(
-    css.success,
-    { [css.toggle]: canVote },
-  );
-  const closeToggle = classNames(
-    css.close,
-    { [css.toggle]: !canVote },
-  );
-  const permissionLevel = getPermissionDisplay(permissions);
-  return (
-    <tr className={userClass}>
-      <td className={css.left}>{name}</td>
-      <td
-        className={css.right}
-        title={`${registeredDate.format('LLL')} (${registeredDate.fromNow()})`}
-      >
-        <div
-          className={classNames(css.action,
-            { [css.success]: completedRegistration,
-              [css.close]: !completedRegistration,
-              [css.toggle]: completedRegistration,
-            },
-        )}
-        >{completedRegistration}</div>
-      </td>
-      <td className={css.right}>
-        {permissionLevel}
-      </td>
-      <td className={css.right}>
-        <button
-          className={css.action}
-          disabled={permissions < CAN_VOTE}
-          onClick={() => toggleCanVote(id, true)}
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { id, name, registered, canVote, completedRegistration, permissions, toggleCanVote,
+    } = this.props;
+    const userClass = classNames({
+      [css.canNotVote]: !canVote,
+    });
+    const registeredDate = moment(registered);
+    const successToggle = classNames(
+      css.success,
+      { [css.toggle]: canVote },
+    );
+    const closeToggle = classNames(
+      css.close,
+      { [css.toggle]: !canVote },
+    );
+    const permissionLevel = getPermissionDisplay(permissions);
+    return (
+      <tr className={userClass}>
+        <td className={css.left}>{name}</td>
+        <td
+          className={css.right}
+          title={`${registeredDate.format('LLL')} (${registeredDate.fromNow()})`}
         >
           <div
-            className={permissions >= CAN_VOTE ? successToggle : css.unavailable}
-            title={permissions >= CAN_VOTE
-              ? 'Gi brukeren stemmerett'
-              : 'Brukeren har ikke rett til å få stemmerett.'}
-          />
-        </button>
-        <button className={css.action} onClick={() => toggleCanVote(id, false)}>
-          <div className={closeToggle} title="Fjern brukerens stemmerett" />
-        </button>
-      </td>
-    </tr>
-  );
-};
+            className={classNames(css.action,
+              { [css.success]: completedRegistration,
+                [css.close]: !completedRegistration,
+                [css.toggle]: completedRegistration,
+              },
+          )}
+          >{completedRegistration}</div>
+        </td>
+        <td className={css.right}>
+          {permissionLevel}
+        </td>
+        <td className={css.right}>
+          <button
+            className={css.action}
+            disabled={permissions < CAN_VOTE}
+            onClick={() => toggleCanVote(id, true)}
+          >
+            <div
+              className={permissions >= CAN_VOTE ? successToggle : css.unavailable}
+              title={permissions >= CAN_VOTE
+                ? 'Gi brukeren stemmerett'
+                : 'Brukeren har ikke rett til å få stemmerett.'}
+            />
+          </button>
+          <button className={css.action} onClick={() => toggleCanVote(id, false)}>
+            <div className={closeToggle} title="Fjern brukerens stemmerett" />
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
 
 User.propTypes = {
   name: PropTypes.string.isRequired,
