2cc5a54efe52de4cc999b62a56e179049eab677a
Christian Duvholt <christian@duvholt.net>
2017-10-31 01:23:56 +0100

    Hash several times to generate new funkier names

--- server/utils/sillyName.js
+++ server/utils/sillyName.js
@@ -1,15 +1,16 @@
-const { randomNoun, randomAdjective } = require('sillyname');
+const sillyname = require('sillyname');
 const SHA256 = require('crypto-js/sha256');
 
-const idNumberGenerator = id => () => parseInt(SHA256(id.toString()).toString(), 16) / (16 ** 64);
+const idNumberGenerator = (id) => {
+  let hashedId = id;
+  return () => {
+    hashedId = SHA256(hashedId.toString()).toString();
+    return parseInt(hashedId, 16) / (16 ** 64);
+  };
+};
 
 function generateStupidName(id) {
-  const generator = idNumberGenerator(id);
-  const noun1 = randomNoun(generator);
-  let noun2 = randomNoun(1 - generator);
-  noun2 = noun2.substr(0, 1).toUpperCase() + noun2.substr(1);
-  const adjective = randomAdjective(generator);
-  return `${adjective}${noun1} ${noun2}`;
+  return sillyname(idNumberGenerator(id));
 }
 
 module.exports = generateStupidName;
