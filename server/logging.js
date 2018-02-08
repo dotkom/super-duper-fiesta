076393503ec373619eea3b864d46b887b02af767
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-02-08 18:23:38 +0100

    Add some more fine grained loglevels for database logging

--- server/logging.js
+++ server/logging.js
@@ -1,11 +1,23 @@
 const winston = require('winston');
 const winstonError = require('winston-error');
 
-const consoleLogger = new (winston.Logger)({});
+const levels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4,
+    silly: 5,
+    database: 6,
+  },
+};
+
+const consoleLogger = new (winston.Logger)({ levels: levels.levels });
 
 if (process.env.NODE_ENV !== 'test') {
   consoleLogger.add(winston.transports.Console, {
-    level: 'debug',
+    level: process.env.SDF_LOGLEVEL || 'database',
     exitOnError: false,
     handleExceptions: true,
     humanReadableUnhandledException: true,
