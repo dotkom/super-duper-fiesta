85ba0f231b09c07db0eebcf71ccea901ab2c6f4f
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-02-11 01:58:02 +0100

    Remove test of oidc-client login redirect logic

--- server/auth/__tests__/index.js
+++ server/auth/__tests__/index.js
@@ -28,47 +28,12 @@ describe('auth', () => {
     app = await getApp();
   });
 
-  it.skip('redirects to auth provider when hitting /login', async () => {
-    process.env.SDF_OIDC_PROVIDER = 'http://example.org/openid';
-    process.env.SDF_OIDC_CLIENT_ID = '123456';
-    const response = await request(app).get('/auth');
-    expect(response.statusCode).toEqual(302);
-    expect(response.header).toEqual(
-      expect.objectContaining({
-        location: expect.stringContaining(process.env.SDF_OIDC_PROVIDER),
-      }),
-    );
-  });
-
   it('redirects to base page after logging out', async () => {
     const response = await request(app).get('/logout');
     expect(response.statusCode).toEqual(302);
     expect(response.header).toEqual(
       expect.objectContaining({
         location: '/',
-      }),
-    );
-  });
-});
-
-describe('openid enabled', () => {
-  beforeEach(async () => {
-    process.env.SDF_OIDC_PROVIDER = 'http://example.org/openid';
-    process.env.SDF_OIDC_CLIENT_ID = '123456';
-  });
-
-  afterEach(() => {
-    process.env.SDF_OIDC_PROVIDER = '';
-    process.env.SDF_OIDC_CLIENT_ID = '';
-  });
-
-  it('redirects to OpenID provider when OIDC enabled', async () => {
-    app = await getApp();
-    const response = await request(app).get('/login');
-    expect(response.statusCode).toEqual(302);
-    expect(response.header).toEqual(
-      expect.objectContaining({
-        location: expect.stringContaining(process.env.SDF_OIDC_PROVIDER),
       }),
     );
   });
