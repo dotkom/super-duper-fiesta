663ff75592a9b6666e3b1884954e2280814edca5
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-02-27 16:51:52 +0100

    Add listener tests for issue

--- server/channels/__tests__/issue.js
+++ server/channels/__tests__/issue.js
@@ -4,13 +4,20 @@ jest.mock('../../models/meeting.accessors');
 jest.mock('../../models/user.accessors');
 jest.mock('../../models/vote.accessors');
 const { addAlternative } = require('../../models/alternative.accessors');
-const { createIssue, closeIssue, adminDeleteIssue, adminDisableVoting, adminEnableVoting } = require('../issue');
+const { listener, createIssue, closeIssue, adminDeleteIssue, adminDisableVoting, adminEnableVoting } = require('../issue');
 const { addIssue, endIssue, deleteIssue, getActiveQuestion, getIssueWithAlternatives, updateIssue } = require('../../models/issue.accessors');
 const { getActiveGenfors, getGenfors } = require('../../models/meeting.accessors');
 const { getQualifiedUsers } = require('../../models/user.accessors');
 const { getVotes } = require('../../models/vote.accessors');
 const { generateSocket, generateIssue, generateGenfors, generateUser, generateManager, generateVote, generateAlternative } = require('../../utils/generateTestData');
 const { VOTING_NOT_STARTED, VOTING_FINISHED } = require('../../../common/actionTypes/issues');
+const {
+  ADMIN_CLOSE_ISSUE,
+  ADMIN_CREATE_ISSUE,
+  ADMIN_DELETE_ISSUE,
+  ADMIN_DISABLE_VOTING,
+  ADMIN_ENABLE_VOTING,
+} = require('../../../common/actionTypes/adminButtons');
 
 beforeEach(() => {
   addAlternative.mockImplementation(async () => generateAlternative());
@@ -143,5 +150,112 @@ describe('adminEnableVoting', () => {
 
     expect(socket.emit.mock.calls).toMatchSnapshot();
     expect(socket.broadcast.emit.mock.calls).toMatchSnapshot();
+  });
+});
+
+function generateSocketData(data) {
+  return {
+    type: ADMIN_CREATE_ISSUE,
+    ...data,
+  };
+}
+
+describe('listener', () => {
+  beforeAll(() => {
+    addIssue.mockImplementation(async () => generateIssue());
+    getIssueWithAlternatives.mockImplementation(async () => generateIssue());
+    getActiveGenfors.mockImplementation(async () => generateGenfors());
+    getGenfors.mockImplementation(async () => generateGenfors());
+    getQualifiedUsers.mockImplementation(async () => 0);
+    updateIssue.mockImplementation(async (_, data) => generateIssue(data));
+  });
+
+  it('listens to ADMIN_CREATE_ISSUE', async (done) => {
+    const socket = generateSocket();
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: ADMIN_CREATE_ISSUE, data: generateIssue() }));
+
+    setTimeout(() => {
+      expect(socket.emit).toBeCalled();
+      expect(socket.broadcast.emit).toBeCalled();
+      done();
+    });
+  });
+
+  it('listens to ADMIN_CLOSE_ISSUE', async (done) => {
+    endIssue.mockImplementation(async data => generateIssue({ ...data, status: VOTING_FINISHED }));
+    getVotes.mockImplementation(async ({ id: issueId }) => [
+      generateVote({ issueId, id: '1' }),
+      generateVote({ issueId, id: '2' }),
+      generateVote({ issueId, id: '3' }),
+      generateVote({ issueId, id: '4' }),
+    ]);
+    const socket = generateSocket({ permissions: 10 });
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: ADMIN_CLOSE_ISSUE, data: { issue: generateIssue() } }));
+
+    setTimeout(() => {
+      expect(socket.emit).toBeCalled();
+      expect(socket.broadcast.emit).toBeCalled();
+      done();
+    });
+  });
+
+  it('listens to ADMIN_DELETE_ISSUE', async (done) => {
+    deleteIssue.mockImplementation(async data => generateIssue({ ...data, active: false }));
+    getVotes.mockImplementation(async ({ id: issueId }) => [
+      generateVote({ issueId, id: '1' }),
+      generateVote({ issueId, id: '2' }),
+      generateVote({ issueId, id: '3' }),
+      generateVote({ issueId, id: '4' }),
+    ]);
+    const socket = generateSocket({ permissions: 10 });
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: ADMIN_DELETE_ISSUE, data: { issue: generateIssue() } }));
+
+    setTimeout(() => {
+      expect(socket.emit).toBeCalled();
+      expect(socket.broadcast.emit).toBeCalled();
+      done();
+    });
+  });
+
+  it('listens to ADMIN_DISABLE_VOTING', async (done) => {
+    const socket = generateSocket({ permissions: 10 });
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: ADMIN_DISABLE_VOTING, data: { issue: generateIssue() } }));
+
+    setTimeout(() => {
+      expect(socket.emit).toBeCalled();
+      expect(socket.broadcast.emit).toBeCalled();
+      done();
+    });
+  });
+
+  it('listens to ADMIN_ENABLE_VOTING', async (done) => {
+    const socket = generateSocket({ permissions: 10 });
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: ADMIN_ENABLE_VOTING, data: { issue: generateIssue() } }));
+
+    setTimeout(() => {
+      expect(socket.emit).toBeCalled();
+      expect(socket.broadcast.emit).toBeCalled();
+      done();
+    });
+  });
+
+  it('ignores INVALID_ACTION', async () => {
+    const socket = generateSocket({ completedRegistration: false });
+    await listener(socket);
+
+    await socket.mockEmit('action', generateSocketData({ type: 'INVALID_ACTION' }));
+
+    expect(socket.emit).not.toBeCalled();
+    expect(socket.broadcast.emit).not.toBeCalled();
   });
 });
